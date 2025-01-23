3c9a798da4804dbbeab54fd73f2adc3bd4ce0143
Yogeshkumar Hadiya <yghadiya33@gmail.com>
2025-01-23 15:23:26 +0530

    change base url

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,6 +1,6 @@
 // API URLs
-export const BASE_API_URL = 'https://localhost:7009';
-// export const BASE_API_URL = 'https://dotnet9api.azurewebsites.net';
+// export const BASE_API_URL = 'https://localhost:7009';
+export const BASE_API_URL = 'https://dotnet9api.azurewebsites.net';
 export const SIGN_IN_URL = `${BASE_API_URL}/auth/sign-in`;
 export const SIGN_UP_URL = `${BASE_API_URL}/auth/register`;
 export const TODOS_BASE_URL = `${BASE_API_URL}/todos`;
